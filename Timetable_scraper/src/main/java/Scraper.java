1deb6355c503d745311e0c57d950e19ff458cfff
m-wlodarczyk <mich.j.wlodarczyk@gmail.com>
2018-03-06 13:16:20 +0100

    Adding dayDivider class and modifying scraper

--- Timetable_scraper/src/main/java/Scraper.java
+++ Timetable_scraper/src/main/java/Scraper.java
@@ -1,18 +1,15 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import java.util.ArrayList;
 
 public class Scraper {
     public static void main(String[] args) throws Exception {
         final Document document = Jsoup.connect("http://www.mpk.poznan.pl/component/transport/16/SZYM42/").get();
-
+        ArrayList<String> rows = new ArrayList<String>();
         for (Element row : document.select("table.timetable tr")){
-            final String Hour = row.select(".MpkHours").text();
-            final String Minutes = row.select(".MpkMinutes").text();
-            if (Hour.isEmpty() || Minutes.isEmpty()){
-                continue;
-            }
-            System.out.println(Hour + " : " + Minutes);
+            final String one_row = row.text();
+            rows.add(one_row);
         }
     }
 }
---
+++ Timetable_scraper/src/main/java/dayDivider.java
@@ -0,0 +1,13 @@
+public class dayDivider {
+    String regular, saturdays, sundays;
+
+    dayDivider(){
+        regular = null;
+        saturdays = null;
+        sundays = null;
+    }
+
+    void divide(String undivided) {
+        
+    }
+}
