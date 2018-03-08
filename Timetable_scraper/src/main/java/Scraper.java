c78396d5e6af8173ae2abd8c286fd46459b70193
m-wlodarczyk <mich.j.wlodarczyk@gmail.com>
2018-03-08 16:26:10 +0100

    Improving scraping and creating 3 HashMaps to store timetables for workdays, saturdays and sundays.

--- Timetable_scraper/src/main/java/Scraper.java
+++ Timetable_scraper/src/main/java/Scraper.java
@@ -2,14 +2,46 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Scraper {
     public static void main(String[] args) throws Exception {
         final Document document = Jsoup.connect("http://www.mpk.poznan.pl/component/transport/16/SZYM42/").get();
-        ArrayList<String> rows = new ArrayList<String>();
-        for (Element row : document.select("table.timetable tr")){
+        ArrayList<String> Hours = new ArrayList<String>();
+        ArrayList<String> Minutes = new ArrayList<String>();
+        Map<String, String> workdays = new HashMap<String, String>();
+        Map<String, String> saturdays = new HashMap<String, String>();
+        Map<String, String> sundays = new HashMap<String, String>();
+
+        for (Element row : document.select("td.MpkHours")){
             final String one_row = row.text();
-            rows.add(one_row);
+            Hours.add(one_row);
         }
+        for (Element row : document.select("td.MpkMinutes")){
+            final String one_row = row.text();
+            Minutes.add(one_row);
+            //System.out.println(one_row);
+        }
+        for (int i=0, j=0 ; i<Hours.size(); i++, j++) {
+            if (j>2){
+                j=0;
+            }
+            switch (j){
+                case 0:
+                    workdays.put(Hours.get(i), Minutes.get(i));
+                    break;
+                case 1:
+                    saturdays.put(Hours.get(i), Minutes.get(i));
+                    break;
+                case 2:
+                    sundays.put(Hours.get(i), Minutes.get(i));
+                    break;
+            }
+        }
+
+        System.out.println(workdays);
+        System.out.println(saturdays);
+        System.out.println(sundays);
     }
 }
