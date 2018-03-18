f10e866ff2c1327b18a5ab7e6b9ee4fb46e310d2
m-wlodarczyk <mich.j.wlodarczyk@gmail.com>
2018-03-18 22:00:57 +0100

    Creating classes: Lines, Route and Station. Modifying Scraper class to work with added classes. Copying old Scraper class into Scraper_old in case of emergency.

--- Timetable_scraper/src/main/java/mainClass.java
+++ Timetable_scraper/src/main/java/mainClass.java
@@ -4,48 +4,22 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class mainClass {
+
     public static Scraper scraper = new Scraper();
 
-    static void foo() throws IOException {
-        String line, stop;
-        int dir;
-        Map<String, String> tmp = new HashMap<String, String>();
-        scraper.Lines();
+    public static Lines lines = new Lines();
+
+    public static Station station = new Station();
+
+    public static String pickLine() {
         System.out.print("Choose line: ");
-        Scanner s = new Scanner(System.in);
-        line = s.nextLine();
-        scraper.Station();
-        System.out.println("1 - " + scraper.direction.get(0));
-        System.out.println("2 - " + scraper.direction.get(1));
-        dir = Integer.parseInt(s.nextLine());
-        if (dir==1){
-            scraper.printMap(scraper.Left);
-            tmp = scraper.Left;
-        } else if (dir==2){
-            scraper.printMap(scraper.Right);
-            tmp = scraper.Right;
-        }
-        if (dir==1){
-            System.out.println("Pick ur stop");
-            stop = s.next();
-            String ending = line + "/" + scraper.getMapValue(stop, scraper.Left);
-            System.out.println(ending);
-            scraper.Timetable();
-            //System.out.println(scraper.getMapValue(stop, scraper.Left));
-        }
-        else {
-            System.out.println("Pick ur stop");
-            stop = s.nextLine();
-            String ending = line + "/" + scraper.getMapValue(stop, scraper.Right);
-            System.out.println(ending);
-            scraper.Timetable();
-            //System.out.println(scraper.getMapValue(stop, scraper.Right));
-        }
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 
     public static void main(String[] args) throws IOException {
-        scraper.Lines();
-        scraper.Station();
-        scraper.Timetable();
+        scraper.getLines(lines);
+        lines.printLines();
+        scraper.getStations(station, pickLine());
     }
 }
